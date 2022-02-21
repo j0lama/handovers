70b384d1f85eb2a486474657bd8911e0b10f6b0d
j0lama <jon_larrea@hotmail.com>
2022-02-21 12:33:29 +0000

    Cloning from gitlab

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # handovers
+
+This repository has been cloned from https://gitlab.flux.utah.edu/dmaas/srsran-handover/-/tree/master
---
+++ bin/deploy-srs.sh
@@ -0,0 +1,23 @@
+set -ex
+BINDIR=`dirname $0`
+source $BINDIR/common.sh
+
+if [ -f $SRCDIR/srs-setup-complete ]; then
+    echo "setup already ran; not running again"
+    exit 0
+fi
+
+cd $SRCDIR
+git clone $SRS_REPO srsran
+cd srsran
+git checkout release_21_04
+mkdir build
+cd build
+cmake ../
+make -j `nproc`
+sudo make install
+sudo ldconfig
+sudo srsran_install_configs.sh service
+sudo cp /local/repository/etc/srsran/* /etc/srsran/
+
+touch $SRCDIR/srs-setup-complete
